51a6a9becc83fdc64236654052cacd9a18c9fd07
cedricjade0813 <cedricjade13@gmail.com>
2025-09-12 23:43:46 -0700

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection for all includes
 try {
-	$db = new PDO('mysql:host=localhost;dbname=clinic_management_system;charset=utf8mb4', 'root', '');
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = new PDO('mysql:host=127.0.0.1;dbname=clinic_management_system;charset=utf8mb4', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
-	die('Database connection error: ' . $e->getMessage());
+    die('Database connection error: ' . $e->getMessage());
 }
 ?>
